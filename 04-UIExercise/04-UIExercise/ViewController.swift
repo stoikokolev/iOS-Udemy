260462a1e62a3b0072e5f68153222d90b493b7a9
Stoyko Kolev <63014870+stoikokolev@users.noreply.github.com>
2021-01-02 14:34:32 +0200

    First UI Exercise

---
+++ 04-UIExercise/04-UIExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  04-UIExercise
+//
+//  Created by Stoyko Kolev on 2.01.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
